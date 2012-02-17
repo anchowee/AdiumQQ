2598155ab7e9ef51619650eaac87f7b795d96db5
Will Orr <will@worrbase.com>
2012-02-17 04:12:15 -0500

    Removed unnecessary code.

--- AdiumQQ/QQAccount.m
+++ AdiumQQ/QQAccount.m
@@ -14,28 +14,12 @@
     return "prpl-qq";
 }
 
-- (void) configurePurpleAccount {
-    [super configurePurpleAccount];
-}
-
 - (BOOL) canSendFolders {
     return NO;
 }
 
 - (void) beginSendOfFileTransfer:(ESFileTransfer *)fileTransfer {
     return [super _beginSendOfFileTransfer:fileTransfer];
-}
-
-- (void) acceptFileTransferRequest:(ESFileTransfer *)fileTransfer {
-    return [super acceptFileTransferRequest:fileTransfer];
-}
-
-- (void) rejectFileReceiveRequest:(ESFileTransfer *)fileTransfer {
-    return [super rejectFileReceiveRequest:fileTransfer];
-}
-
-- (void) cancelFileTransfer:(ESFileTransfer *)fileTransfer {
-    return [super cancelFileTransfer:fileTransfer];
 }
 
 @end
